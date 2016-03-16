74542a09688b98b839d4b7bee349467e2dffa7bf
Alexandre <cin_aigari@uolinc.com>
2016-03-16 15:16:53 -0300

    Create Project

---
+++ testng-example/src/test/java/TestClass1.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestClass1 extends  BaseApplicationTest{
+
+    @Test
+    public void testMethod1(){
+        System.out.println("testMethod1");
+    }
+
+    @Test
+    public void testMethod2(){
+        System.out.println("testMethod2");
+    }
+}
---
+++ testng-example/src/test/java/TestClass2.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class TestClass2 extends BaseApplicationTest{
+
+    @Test
+    public void testMethod3(){
+        System.out.println("testMethod3");
+    }
+
+    @Test
+    public void testMethod4(){
+        System.out.println("testMethod4");
+    }
+}
